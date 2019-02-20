1781d4a8a0b10df1ca261dc298a318fdf04778a7
Александр Николаев <45828383+alexander89116@users.noreply.github.com>
2019-02-20 21:23:43 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,17 +4,17 @@ new_dir=$1
 if [ -d "$new_dir" ]; 
 then
   	i=1
-	while [ -d "$new_dir$j" ]
+	while [ -d "$new_dir$i" ]
   	do
     		i=$(( $i+1 ))
   	done
 	mv $new_dir $new_dir$i > /dev/null
 fi
-mkdir $new_dir > /dev/null
+mkdir $1 
 archive_name=$2
 for (( i=3; i<=$#; i++ ))
 do
-	all_files=$(find -name "*${!i}" 2> /dev/null)
+	all_files=$(find $HOME -name "*${!i}" 2> /dev/null)
 	cnt=1
 	# echo ${!i}
 	for item in $all_files
@@ -25,12 +25,12 @@ do
 		name1=${name0//".."/"_"}
 		# echo "$name1"
 		new_name=${name1//"/"/".."}
-		cp $item $new_dir > /dev/null
+		cp $item $1 > /dev/null
 		cd $new_dir > /dev/null
 		mv $file $new_name > /dev/null
 		# echo "$cnt $new_name"
 		cnt=$(( $cnt + 1 ))
-		cd > /dev/null
+		cd .. > /dev/null
 	done
 done
 tar -cvzf $archive_name.tar $new_dir > /dev/null
